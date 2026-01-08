5c9d46fae728c656fea8619b03bd948e88ce05b7
yapweijun1996 <yapweijun1996@gmail.com>
2026-01-08 22:04:19 +0800

    New

--- 0_development/pdf-to-jpg/vite.config.ts
+++ 0_development/pdf-to-jpg/vite.config.ts
@@ -6,6 +6,7 @@ import copy from 'rollup-plugin-copy';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
